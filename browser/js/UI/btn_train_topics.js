2993c342f2ea99ea4d297bee9b6590290dc40a30
hong <hxwang@asu.edu>
2017-01-29 14:47:10 -0700

    working on topic trainer

--- browser/js/UI/btn_train_topics.js
+++ browser/js/UI/btn_train_topics.js
@@ -1,10 +1,12 @@
 var $ = require('jquery');
 var train_topics = require('../load/train_topics');
 var test_data = require('../test_data/topic_data');
+var topic_viewer = global.topic_viewer;
 module.exports = $('#btn-train-topics').click(function(){
+  var num_topics = Number($('#input_num_topics').val());
+  var num_iterations = Number($('#input_num_iterations').val());
   // train_topics().then(function(data){
-  //   console.log('topic training finished', JSON.stringify(data, null, 2));
   //   global.topic_viewer.data(data).update();
   // });
-  global.topic_viewer.data(test_data).update();
+  topic_viewer.data(test_data).update();
 });
--- browser/js/load/train_topics.js
+++ browser/js/load/train_topics.js
@@ -1,10 +1,15 @@
 var $ = require('jquery');
 var data;
+var num_topics = 10;
+var num_iterations = 500;
 function callback(data){}
 function load(){
   var deferred = $.ajax({
     url : service_url + '/topictrainer',
-    data : {},
+    data : {
+      num_topics : num_topics,
+      num_iterations : num_iterations
+    },
     dataType: 'json',
     success : function(data){
       callback(data);
@@ -12,10 +17,11 @@ function load(){
   });
   return Promise.resolve(deferred);
 }
-
 function ret(){
   return load();
 }
 ret.callback = function(_){return arguments.length > 0 ? (callback=_,ret):callback;};
 ret.data = function(){return data;};
+ret.num_iterations = function(_){return arguments.length > 0 ? (num_iterations=_,ret):num_iterations;};
+ret.num_topics = function(_){return arguments.length > 0 ? (num_topics=_,ret):num_topics;};
 module.exports = ret;
