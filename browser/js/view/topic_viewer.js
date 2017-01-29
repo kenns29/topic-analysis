5a7d8f4fe9f8d2d3edb0ea9eb5de674ab7815da7
hong <hxwang@asu.edu>
2017-01-29 13:14:59 -0700

    topic viewer fix

--- browser/js/view/topic_viewer.js
+++ browser/js/view/topic_viewer.js
@@ -19,7 +19,7 @@ function update(){
   var topic_enter = topic_sel.enter().append('g').attr('class', 'topic');
   topic_sel.exit().remove();
   var topic_update = graph_g.selectAll('.topic');
-  topic_update.each(function(d){d.height = 0;});
+  topic_update.each(function(d){d.height = 0; d.y = 0;});
   var token_sel = topic_update.selectAll('.token').data(function(d){return d;}, function(d){return d.id;});
   var token_enter = token_sel.enter().append('g').attr('class', 'token');
   token_enter.append('text');
@@ -35,27 +35,28 @@ function update(){
   .attr('text-anchor', 'start').attr('dominant-baseline', 'middle').text(function(d){
     return d.token;
   });
-
+  topic_update.each(function(d, i){
+    if(i > 0){
+      let pre_data = topic_update.data()[i-1];
+      let pre_y = pre_data.y, pre_height = pre_data.height;
+      d.y += pre_y + text_y_space + pre_height;
+    }
+  });
   token_update.each(function(d){d.x = 0;});
   token_update.each(function(d, i){
     d.width = d3.select(this).select('text').node().getComputedTextLength();
     if(i > 0){
-      let pre_x = d3.select(this.parentNode).data()[0][i-1].x;
-      d.x += pre_x + text_x_space + d.width;
+      let pre_data = d3.select(this.parentNode).data()[0][i-1];
+      let pre_x = pre_data.x, pre_width = pre_data.width;
+      d.x += pre_x + text_x_space + pre_width;
     }
-  });
-
-  token_update.attr('transform', function(d, i){
-    return 'translate(' + [d.x, d.height/2] +')';
-  });
-
-  var cum_height = 0;
-  topic_update.each(function(d){
-    d.y = cum_height;
-    cum_height += text_y_space + d.height;
   });
   topic_update.attr('transform', function(d, i){
     return 'translate(' + [0, d.y] +')';
+  });
+  token_update.attr('transform', function(d, i){
+    var topic_height = d3.select(this.parentNode).data()[0].height;
+    return 'translate(' + [d.x, topic_height/2] +')';
   });
   return ret;
 }
