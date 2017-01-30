87e3301f2e30d0570b58842f92db6f11460e34ff
hxwang <hxwang@asu.edu>
2017-01-30 09:35:07 -0700

    ok

--- routes/router_topic_trainer.js
+++ routes/router_topic_trainer.js
@@ -1,8 +1,8 @@
 var get_papers = require('../db_mongo/get_papers');
 var topic_model = require('../mallet/topic_model');
 module.exports = function(req, res){
-  var num_topics = req.params.num_topics;
-  var num_iterations = req.params.num_iterations;
+  var num_topics = Number(req.query.num_topics);
+  var num_iterations = Number(req.query.num_iterations);
   console.log('num_topics', num_topics, 'num_iterations', num_iterations);
   get_papers().then(function(data){
     topic_model.num_iterations(num_iterations).num_topics(num_topics);
