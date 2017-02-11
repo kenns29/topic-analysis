7533a43a9a289893fc9255d55d69e30efc36124d
hxwang <hxwang@asu.edu>
2017-02-11 14:39:06 -0700

    ok

--- db_mongo/get_panels.js
+++ db_mongo/get_panels.js
@@ -8,10 +8,12 @@ module.exports = exports = function(){
   var data;
   var year = 1979;
   var type = DOC.A;
+  var to_year = -1;
   function load(){
     var query = {};
     if(year >= 0) query.year = year;
     if(type >= 0) query.type = type;
+    if(to_year > 0) query.year = {$gte:year,$lt:to_year};
     return co(function*(){
       var db = yield mongo_client.connect(url);
       var col = db.collection('panels');
@@ -26,6 +28,7 @@ module.exports = exports = function(){
   ret.data = function(){return data;};
   ret.year = function(_){return arguments.length > 0 ? (year = _, ret) : year;};
   ret.type = function(_){return arguments.length > 0 ? (type = _, ret) : type;};
+  ret.to_year = function(_){return arguments.length > 0 ?(to_year =_, ret) : to_year;};
   ret.load = load;
   return ret;
 };
--- routes/router_load_papers.js
+++ routes/router_load_papers.js
@@ -10,28 +10,13 @@ module.exports = exports = function(req, res){
   var model_id = Number(req.query.model_id);
   var field = Number(req.query.field);
   var year = Number(req.query.year);
+  var to_year = Number(req.query.to_year); if(!to_year) to_year = -1;
   var type = Number(req.query.type);
-  var get_papers = GetPapers().year(year).type(type);
+  var get_papers = GetPapers().year(year).to_year(to_year).type(type);
   var token_field = field === DOC.TITLE ? 'title_tokens' : 'abstract_tokens';
   get_papers().then(function(data){
-    if(model_id){
-      return co(function*(){
-        var db = yield MongoClient.connect(ConnStat().url());
-        var col = db.collection(model_col);
-        var data_array = yield col.find({id : model_id}).toArray();
-        db.close();
-        var m = data_array[0];
-        let topic_model = TopicModel().load_from_binary(m.model.buffer);
-        let id2pos2token = topic_model.id2pos2token(data, token_field);
-        let id2distr = topic_model.id2distr();
-        let id2tokens = topic_model.id2tokens(id2pos2token);
-        data.forEach(function(d){
-          d.topic_distr = id2distr[d.id];
-          d.topic_tokens = id2tokens[d.id];
-        });
-        return Promise.resolve(data);
-      });
-    } else return Promise.resolve(data);
+    if(model_id) return model_data_promise(model_id);
+    else return Promise.resolve(data);
   }).then(function(data){
     res.json(data);
   }).catch(function(err){
@@ -39,3 +24,22 @@ module.exports = exports = function(req, res){
     res.send(err);
   });
 };
+
+function model_data_promise(model_id){
+  return co(function*(){
+    var db = yield MongoClient.connect(ConnStat().url());
+    var col = db.collection(model_col);
+    var data_array = yield col.find({id : model_id}).toArray();
+    db.close();
+    var m = data_array[0];
+    let topic_model = TopicModel().load_from_binary(m.model.buffer);
+    let id2pos2token = topic_model.id2pos2token(data, token_field);
+    let id2distr = topic_model.id2distr();
+    let id2tokens = topic_model.id2tokens(id2pos2token);
+    data.forEach(function(d){
+      d.topic_distr = id2distr[d.id];
+      d.topic_tokens = id2tokens[d.id];
+    });
+    return Promise.resolve(data);
+  });
+}
