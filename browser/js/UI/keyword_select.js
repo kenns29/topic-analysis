340c39d7a5def20802f61be5a3089d4dcd226a9e
hxwang <hxwang@asu.edu>
2017-02-14 15:55:47 -0700

    ok

--- browser/js/UI/keyword_select.js
+++ browser/js/UI/keyword_select.js
@@ -3,7 +3,13 @@ var d3 = require('../load_d3');
 var DOC = require('../../../flags/doc_flags');
 var LoadTfidf = require('../load/load_tfidf');
 var LoadKeywordTimelineData = require('../load/load_keyword_timeline_data');
+var UpdateKeywordDocumentViewer = require('../control/update_keyword_document_viewer');
+var KeywordSelectControls = require('./keyword_select_controls');
 var co = require('co');
+var get_flags = KeywordSelectControls.get_flags;
+var str2flag = KeywordSelectControls.str2flag;
+var disable_opts = KeywordSelectControls.disable_opts;
+var enable_opts = KeywordSelectControls.enable_opts;
 module.exports = exports = function(){
   var data;
   function update(_data){
@@ -45,18 +51,30 @@ module.exports = exports = function(){
   ret.init = init;
   ret.load = load;
   ret.update = update;
+  ret.get_flags = get_flags;
+  ret.str2flag = str2flag;
+  ret.disable_opts = disable_opts;
+  ret.enable_opts = enable_opts;
   return ret;
 };
 function update_all_keyword_timeline(){
   var flag = get_flags();
   var data = global.multi_keyword_timeline.data();
+  var loading = global.multi_keyword_timeline.loading();
   co(function*(){
+    $(loading).show();
     for(let i = 0; i < data.length; i++){
       let keyword = data[i].id;
       let line_data = yield LoadKeywordTimelineData().type(flag.type).level(flag.level).load(keyword);
       global.multi_keyword_timeline.replace_timeline(line_data);
     }
+    $(loading).hide();
     global.multi_keyword_timeline.update();
+    var brushes = global.multi_keyword_timeline.brushes();
+    if(brushes.is_activated()){
+      UpdateKeywordDocumentViewer().keywords(data.map(function(d){return d.id;}))
+      .update_domain(global.multi_keyword_timeline.brushes().domain());
+    }
   }).catch(function(err){
     console.log(err);
   });
@@ -72,45 +90,4 @@ function insert_keyword_timeline(keyword){
       console.log(err);
     });
   }
-}
-module.exports.get_flags = get_flags;
-module.exports.str2flag = str2flag;
-module.exports.disable_opts = disable_opts;
-module.exports.enable_opts = enable_opts;
-function get_flags(){
-  var level_str = $('#keyword-select-div #select-level').val();
-  var type_str = $('#keyword-select-div #select-type').val();
-  var field_str = $('#keyword-select-div #select-field').val();
-  var level = str2flag(level_str);
-  var type = str2flag(type_str);
-  var field = str2flag(field_str);
-  return {level:level,type:type,field:field};
-}
-function str2flag(str){
-  switch(str){
-    case 'P' : return DOC.P;
-    case 'PN' : return DOC.PN;
-    case 'A' : return DOC.A;
-    case 'RW' : return DOC.RW;
-    case 'TITLE' : return DOC.TITLE;
-    case 'ABSTRACT' : return DOC.ABSTRACT;
-    case 'ALL' : return -1;
-    default : return -1;
-  }
-}
-function disable_opts(){
-  var level_sel = $('#keyword-select-div #select-level');
-  var type_sel = $('#keyword-select-div #select-type');
-  var field_sel = $('#keyword-select-div #select-field');
-  level_sel.prop('disabled', true), level_sel.css('opacity', 0.3);
-  type_sel.prop('disabled', true), type_sel.css('opacity', 0.3);
-  field_sel.prop('disabled', true), field_sel.css('opacity', 0.3);
-}
-function enable_opts(){
-  var level_sel = $('#keyword-select-div #select-level');
-  var type_sel = $('#keyword-select-div #select-type');
-  var field_sel = $('#keyword-select-div #select-field');
-  level_sel.prop('disabled', false), level_sel.css('opacity', 1);
-  type_sel.prop('disabled', false), type_sel.css('opacity', 1);
-  field_sel.prop('disabled', false), field_sel.css('opacity', 1);
 }
