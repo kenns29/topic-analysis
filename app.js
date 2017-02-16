6a7f7bbefeaae13c873b87bc82727979bde52ace
hxwang <hxwang@asu.edu>
2017-02-15 23:06:18 -0700

    working

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 var routes = require('./routes/index');
 var morgan = require('morgan');
+var passport = require('passport');
+var flash = require('connect-flash');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var fs = require('fs');
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -15,9 +19,19 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 var access_log_stream = fs.createWriteStream(path.join(__dirname, 'access.log'),{flags: 'a'});
 app.use(morgan('combined', {stream : access_log_stream}));
+
+// required for passport
+app.use(session({
+  secret: 'secretsecretsession',
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
 app.use('/', routes);
 app.listen(10082, function () {
   console.log('Example app listening on port 10082!');
