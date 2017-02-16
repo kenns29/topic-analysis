14aa092f199fa0522e4297160dcb0c0bb8a7c2df
hxwang <hxwang@asu.edu>
2017-02-15 23:19:18 -0700

    session

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var routes = require('./routes/index');
 var morgan = require('morgan');
 var passport = require('passport');
 var flash = require('connect-flash');
-var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var fs = require('fs');
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +19,6 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 var access_log_stream = fs.createWriteStream(path.join(__dirname, 'access.log'),{flags: 'a'});
 app.use(morgan('combined', {stream : access_log_stream}));
@@ -27,6 +26,8 @@ app.use(morgan('combined', {stream : access_log_stream}));
 // required for passport
 app.use(session({
   secret: 'secretsecretsession',
+  resave : false,
+  saveUninitialized : false
 })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
