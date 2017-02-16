d46cba56f027bbb09c2f5d9ba73e29b20e4941b7
hxwang <hxwang@asu.edu>
2017-02-15 17:04:32 -0700

    ok

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var multer = require('multer'); // v1.0.5
 var upload = multer(); // for parsing multipart/form-data
 var routes = require('./routes/index');
+var morgan = require('morgan');
+var fs = require('fs');
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
@@ -14,6 +16,8 @@ app.set('view engine', 'html');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname, 'public')));
+var access_log_stream = fs.createWriteStream(path.join(__dirname, 'access.log'),{flags: 'a'});
+app.use(morgan('combined', {stream : access_log_stream}));
 app.use('/', routes);
 app.listen(10082, function () {
   console.log('Example app listening on port 10082!');
