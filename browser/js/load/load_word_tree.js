dfee92d3d37b2994c1b50781fb7865e5c1f6b841
hxwang <hxwang@asu.edu>
2017-02-18 17:04:35 -0700

    flags for word tree load

--- browser/js/load/load_word_tree.js
+++ browser/js/load/load_word_tree.js
@@ -1,5 +1,6 @@
 var $ = require('jquery');
 var DOC = require('../../../flags/doc_flags');
+var DIRECTION = require('../../../flags/word_tree_direction_flags');
 var keywords2str = require('./keywords2str');
 module.exports = exports = function(){
   var data;
@@ -10,11 +11,13 @@ module.exports = exports = function(){
   var field = DOC.TITLE;
   var keywords;
   var root_word = 'womens';
+  var direction = DIRECTION.FORWARD;
   function callback(data){}
   function load(){
     var deferred = $.ajax({
       url : service_url + '/loadwordtree',
-      data : {root_word: root_word,year:year,to_year:to_year,type:type,field:field,keywords:keywords2str(keywords)},
+      data : {root_word: root_word,year:year,to_year:to_year,type:type,field:field,
+        keywords:keywords2str(keywords), direction:direction},
       dataType: 'json',
       success : function(data){
         callback(data);
@@ -32,5 +35,7 @@ module.exports = exports = function(){
   ret.type = function(_){return arguments.length > 0 ? (type = _, ret) : type;};
   ret.field = function(_){return arguments.length > 0 ? (field = _, ret) : field;};
   ret.keywords = function(_){return arguments.length > 0 ? (keywords = _, ret) : keywords;};
+  ret.root_word = function(_){return arguments.length > 0 ? (root_word = _, ret) : root_word;};
+  ret.direction = function(_){return arguments.length > 0 ? (direction = _, ret) : direction;};
   return ret;
 };
--- routes/router_load_word_tree.js
+++ routes/router_load_word_tree.js
@@ -2,6 +2,7 @@ var GetPapers = require('../db_mongo/get_papers');
 var co = require('co');
 var ConnStat = require('../db_mongo/connection');
 var DOC = require('../flags/doc_flags');
+var DIRECTION = require('../flags/word_tree_direction_flags');
 var WordTree = require('../nlp_word_tree/word_tree');
 var co = require('co');
 module.exports = exports = function(req, res){
@@ -10,6 +11,8 @@ module.exports = exports = function(req, res){
   var to_year = Number(req.query.to_year); if(!to_year) to_year = -1;
   var type = Number(req.query.type);
   var level = Number(req.query.level);
+  var direction = Number(req.query.direction);
+  var root_word = req.query.root_word;
   var keywords = [];
   var keywords_str = req.query.keywords;
   if(keywords_str) keywords = keywords_str.split(',');
@@ -18,7 +21,9 @@ module.exports = exports = function(req, res){
   var token_field = field === DOC.TITLE ? 'title_tokens' : 'abstract_tokens';
   co(function*(){
     var data = yield get_fun();
-    var word_tree = WordTree().create(data);
+    var word_tree = WordTree().root_word(root_word);
+    if(direction === DIRECTION.FORWARD) word_tree.reverse(false).create(data);
+    else if(direction === DIRECTION.REVERSE) word_tree.reverse(true).create(data);
     res.json(word_tree.root());
   }).catch(function(err){
     console.log(err);
