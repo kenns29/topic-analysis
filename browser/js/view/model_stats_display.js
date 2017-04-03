96fba81b597c921edbe7642e68c56195833fa70e
hxwang <hxwang@asu.edu>
2017-04-03 13:02:15 -0700

    ok

--- browser/js/view/model_stats_display.js
+++ browser/js/view/model_stats_display.js
@@ -7,10 +7,12 @@ var LoadPapers = require('../load/load_papers');
 var LoadPanels = require('../load/load_panels');
 var DOC = require('../../../flags/doc_flags');
 var co = require('co');
+var controller_topic_model_selection = require('../control/controller_topic_model_selection')();
 var container = '#topic-model-display-div';
 var data = [];
 var table;
 var selected_model = {};
+var model_update;
 function init(){
   table = d3.select(container).append('table')
   .attr('class', 'table')
@@ -55,7 +57,7 @@ function update(){
   .append('div').style('text-align', 'center')
   .append('i').attr('class', 'fa fa-trash-o fa-cog fa-1x').style('cursor', 'pointer');
   model_sel.exit().remove();
-  var model_update = model_sel.merge(model_enter);
+  model_update = model_sel.merge(model_enter);
   model_update.select('.name').html(function(d){return d.name;});
   model_update.select('.year').html(function(d){return d.year;});
   model_update.select('.type').html(function(d){return flag2str(d.type);});
@@ -63,31 +65,33 @@ function update(){
   model_update.select('.field').html(function(d){return flag2str(d.field);});
   model_update.select('.num-topics').html(function(d){return d.num_topics;});
   model_update.select('.radio-td').select('.radio').select('input').on('click', function(d){
-    selected_model = d;
-    model_update.filter(function(g){
-      return g.id !== d.id;
-    }).select('.radio-td').select('.radio').select('input').each(function(){
-      this.checked = false;
-    });
-    co(function*(){
-      $(global.topic_viewer.loading()).show();
-      var topics = yield LoadTopicModel().id(d.id).load();
-      $(global.topic_viewer.loading()).hide();
-      yield global.topic_viewer.display_opt('weight').data(topics).update();
-      var data = yield global.topic_document_viewer.year(d.year).type(d.type).level(d.level).load();
-      global.topic_document_viewer.data(data).update();
-    }).catch(function(err){
-      console.log(err);
-      $(global.topic_viewer.loading()).hide();
-    });
+    controller_topic_model_selection.select_model(d);
+    // selected_model = d;
+    // model_update.filter(function(g){
+    //   return g.id !== d.id;
+    // }).select('.radio-td').select('.radio').select('input').each(function(){
+    //   this.checked = false;
+    // });
+    // co(function*(){
+    //   $(global.topic_viewer.loading()).show();
+    //   var topics = yield LoadTopicModel().id(d.id).load();
+    //   $(global.topic_viewer.loading()).hide();
+    //   yield global.topic_viewer.display_opt('weight').data(topics).update();
+    //   var data = yield global.topic_document_viewer.year(d.year).type(d.type).level(d.level).load();
+    //   global.topic_document_viewer.data(data).update();
+    // }).catch(function(err){
+    //   console.log(err);
+    //   $(global.topic_viewer.loading()).hide();
+    // });
   });
   model_update.select('.trash').select('i').on('click', function(d, i){
-    DeleteTopicModel().id(d.id).load().then(function(status){
-        data.splice(i, 1); update();
-    }).catch(function(err){
-      console.log(err);
-      alert('did not delete the model successfully');
-    });
+    controller_topic_model_selection.delete_model(d);
+    // DeleteTopicModel().id(d.id).load().then(function(status){
+    //     data.splice(i, 1); update();
+    // }).catch(function(err){
+    //   console.log(err);
+    //   alert('did not delete the model successfully');
+    // });
   });
   return ret;
 }
@@ -109,6 +113,7 @@ var ret = {};
 ret.load = load;
 ret.init = init;
 ret.update = update;
+ret.d3models = function(){return model_update;};
 ret.data = function(_){return arguments.length>0 ? (data =_, ret) : data;};
-ret.selected_model = function(){return selected_model;};
+ret.selected_model = function(_){return arguments.length > 0 ? (selected_model = _, ret) : selected_model;};
 module.exports = init();
