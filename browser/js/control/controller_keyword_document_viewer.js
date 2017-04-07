e27d2a5a5b1a52f1ffc935b252333282e70287e8
hxwang <hxwang@asu.edu>
2017-04-07 14:26:15 -0700

    ok

--- browser/js/control/controller_keyword_document_viewer.js
+++ browser/js/control/controller_keyword_document_viewer.js
@@ -1,16 +1,17 @@
 var KeywordSelectControls = require('../UI/keyword_select_controls');
-var safe_load = require('../safe_load')();
+var SafeLoad = require('../safe_load')();
 module.exports = exports = function(){
+  var safe_load = SafeLoad();
   var keywords = [];
   var year = -1, to_year = -1;
   function ret(){}
-  ret.update = function(){update(keywords, [year, to_year]);};
+  ret.update = function(){update(keywords, [year, to_year], safe_load);};
   ret.keywords = function(_){return arguments.length > 0 ? (keywords = _, ret) : keywords;};
   ret.year = function(_){return arguments.length > 0 ? (year = _, ret) : year;};
   ret.to_year = function(_){return arguments.length > 0 ? (to_year = _, ret) : to_year;};
   return ret;
 };
-function update(keywords, domain){
+function update(keywords, domain, safe_load){
   var flags = KeywordSelectControls.get_flags();
   var load = global.keyword_document_viewer.keywords(keywords)
   .year(domain[0]).to_year(domain[1])
