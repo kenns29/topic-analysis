6e9a0f2601c09960210904a0fd54f7319b48608a
hxwang <hxwang@asu.edu>
2017-04-18 14:30:24 -0700

    and or logic for word

--- db_mongo/keyword_data_promise.js
+++ db_mongo/keyword_data_promise.js
@@ -1,4 +1,5 @@
 var co = require('co');
+var word_combo = require('./word_combo')();
 module.exports = exports = function keyword_data_promise(data, keywords, token_field){
   return co(function*(){
     data.forEach(function(d){
@@ -12,8 +13,12 @@ module.exports = exports = function keyword_data_promise(data, keywords, token_f
 function keyword_tokens(tokens, keywords){
   var keyword_tokens = [];
   var key2keyword = [];
-  keywords.forEach(function(keyword){
-    key2keyword[keyword.toLowerCase()] = keyword;
+  keywords.forEach(function(hr){
+    var ur = word_combo.hr2ur(hr);
+    var words = ur.split(/[&\/\(\)]/);
+    words.forEach(function(keyword){
+      key2keyword[keyword.toLowerCase()] = keyword;
+    });
   });
   tokens.forEach(function(d){
     if(key2keyword.hasOwnProperty(d.lemma.toLowerCase())) keyword_tokens.push(d);
