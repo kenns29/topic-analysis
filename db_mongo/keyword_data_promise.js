d3894c94289001762d433f910cb295ae2cd3236c
hxwang <hxwang@asu.edu>
2017-04-18 16:02:17 -0700

    wild card highlighting

--- db_mongo/keyword_data_promise.js
+++ db_mongo/keyword_data_promise.js
@@ -14,16 +14,29 @@ function keyword_tokens(tokens, keywords){
   var keyword_tokens = [];
   var key2keyword = [];
   var re = new RegExp();
+  var re_str = '';
   keywords.forEach(function(hr){
     var ur = word_combo.hr2ur(hr);
     var words = ur.split(/[&\/\(\)]/);
     words.forEach(function(keyword){
-      if(keyword.match())
-      key2keyword[keyword.toLowerCase()] = keyword;
+      if(keyword.match('%')){
+        if(re_str === ''){
+          re_str += word_combo.word2re_str(keyword);
+        } else {
+          re_str += '|' + word_combo.word2re_str(keyword);
+        }
+      } else {
+        key2keyword[keyword.toLowerCase()] = keyword;
+      }
     });
   });
+  re.compile(re_str, 'i');
   tokens.forEach(function(d){
-    if(key2keyword.hasOwnProperty(d.lemma.toLowerCase())) keyword_tokens.push(d);
+    if(key2keyword.hasOwnProperty(d.lemma.toLowerCase()))
+      keyword_tokens.push(d);
+    else if(re_str != '' && d.lemma.match(re)){
+      keyword_tokens.push(d);
+    }
   });
   return keyword_tokens;
 }
