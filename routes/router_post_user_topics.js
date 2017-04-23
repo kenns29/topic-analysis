9eb706f7840ca4f98a89e49fda019edcc5789802
hxwang <hxwang@asu.edu>
2017-04-23 10:00:37 -1000

    ok

--- routes/router_post_user_topics.js
+++ routes/router_post_user_topics.js
@@ -3,7 +3,7 @@ module.exports = exports = post;
 function post(passport){
   return function(req, res){
     var text = req.file.buffer.toString('utf8');
-
+    console.log('text', text);
     res.send('success');
   };
 }
