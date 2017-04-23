d0ced258874040689c67cb9850f58b5a5cda0666
hxwang <hxwang@asu.edu>
2017-04-22 22:39:58 -1000

    ok

--- routes/router_post_user_topics.js
+++ routes/router_post_user_topics.js
@@ -2,10 +2,8 @@ var btoa = require('btoa');
 module.exports = exports = post;
 function post(passport){
   return function(req, res){
-    var buffer = req.file.buffer;
-    var encoding = req.file.encoding;
-    var text = buffer.toString(encoding);
-    console.log('text', text);
+    var text = req.file.buffer.toString('utf8');
+
     res.send('success');
   };
 }
